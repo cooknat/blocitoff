00ddea07aae945529880a6abcb7034a82ab4479a
Natalie Cook <natalie.t.cook@gmail.com>
2017-12-12 13:49:24 +0000

    production url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,4 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
   
-   protected
-    def after_sign_in_path_for(resource)
-      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    end
-  
 end
