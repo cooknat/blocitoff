28f8425dda4e0589418f9cdac3a744989215d981
Natalie Cook <natalie.t.cook@gmail.com>
2017-12-12 11:24:02 +0000

    extra cred

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
   
+   protected
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    end
+  
 end
